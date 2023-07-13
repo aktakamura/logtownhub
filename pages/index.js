7daf018675f81750f4b495b571ff253319947d76
aktakamura <138618414+aktakamura@users.noreply.github.com>
2023-07-13 20:47:04 +0000

    Adicionado um parágrafo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Obrigado por ser o meu porto seguro e estar sempre comigo! Amo você!</h1>
+    return <><h1>Obrigado por ser o meu porto seguro e estar sempre comigo! Amo você!</h1>
+        <p>Este é o primeiro passo para eu aprimorar as minhas skills de dev e montar a página do Motto Fukushima!</p>
+    </>
 }
 
 export default Home;
