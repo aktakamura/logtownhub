ba3a95a5e194afe00e5e07531e077b63d93c2d43
aktakamura <138618414+aktakamura@users.noreply.github.com>
2023-07-13 20:57:44 +0000

    alteração do texto para relembrar o fluxo de commandos

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
     return <><h1>Obrigado por ser o meu porto seguro e estar sempre comigo! Amo você!</h1>
-        <p>Este é o primeiro passo para eu aprimorar as minhas skills de dev e montar a página do Motto Fukushima!</p>
+        <p>Este é o primeiro passo para eu aprimorar as minhas skills de dev e montar a página do Motto Fukushima!!</p>
     </>
 }
 
