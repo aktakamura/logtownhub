91b71240ccfb1b4a0c8491677fa3fcf6b4cafc49
aktakamura <aktakamura@gmail.com>
2023-07-15 21:30:36 +0000

    teste para correção de erros

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <>
       <h1>
-        Obrigado por ser o meu porto seguro e estar sempre comigo! Amo você!
+        Obrigado por ser o meu porto seguro e estar sempre comigo! Amo você!!
       </h1>
       <p>
         Este é o primeiro passo para eu aprimorar as minhas skills de dev e
